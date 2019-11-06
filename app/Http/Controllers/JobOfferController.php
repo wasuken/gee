cc82e71f0cf4e3f5c2bf9e732f2fa132d1ade2d0
wasuken <wevorence@gmail.com>
2019-11-06 12:11:05 +0900

    impl search function.

--- app/Http/Controllers/JobOfferController.php
+++ app/Http/Controllers/JobOfferController.php
@@ -6,13 +6,24 @@ use Illuminate\Http\Request;
 use \App\JobOffer;
 use \App\Corp;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class JobOfferController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $offers = JobOffer::all();
+        $offers = DB::table('job_offers');
         $user = Auth::user();
-        return view('job_offer/index', ['offers' => $offers, 'user' => $user]);
+        if($request->occupation !== null && $request->occupation !== '') {
+            $offers = $offers->where('occupation', 'like', '%' . $request->occupation . '%');
+        }
+        if($request->work_location !== null && $request->work_location !== '') {
+            $offers = $offers->where('work_location', 'like','%' . $request->work_location . '%');
+        }
+        if($request->keyword !== null && $request->keyword !== '') {
+            $offers = $offers->where('contents', 'like', '%' . $request->keyword . '%');
+        }
+        return view('job_offer/index', ['offers' => $offers->get()->all(), 'user' => $user]);
     }
 }
